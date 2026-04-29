638ea22be65805159895f1b5c5e5e51292b8977a
dylan463 <dylanhinesang@gmail.com>
2026-04-29 21:59:03 +0300

    notif update

--- backend/notif/urls.py
+++ backend/notif/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NotificationView
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register("/",NotificationView, basename="notifications")
+router.register("",NotificationView, basename="notifications")
 
 urlpatterns = router.urls
